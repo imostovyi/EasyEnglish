1675feb9d6ecab0ce97c719a8ebc89f8e2f12a93
imostoviy <i_mostoviy@ukr.net>
2019-03-04 13:55:39 +0200

    Add CoreData

---
+++ EasyEnglish/CoreDataStack.swift
@@ -0,0 +1,29 @@
+//
+//  CoreDataStack.swift
+//  EasyEnglish
+//
+//  Created by Мостовий Ігор on 3/4/19.
+//  Copyright © 2019 Мостовий Ігор. All rights reserved.
+//
+
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    private init() {}
+    
+    private(set) lazy var persistantContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "EasyEnglish")
+        container.loadPersistentStores(completionHandler: { (persistentStoreDescription, error) in
+            if let error = error {
+                debugPrint(error)
+                return
+            }
+            debugPrint(persistentStoreDescription)
+            container.viewContext.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
+        })
+        return container
+    }()
+}
---
+++ EasyEnglish/Entities/Words+CoreDataClass.swift
@@ -0,0 +1,28 @@
+//
+//  Words+CoreDataClass.swift
+//  EasyEnglish
+//
+//  Created by Мостовий Ігор on 3/4/19.
+//  Copyright © 2019 Мостовий Ігор. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Words)
+public class Words: NSManagedObject {
+
+    class func fetchAll() -> [Words] {
+        let context: NSManagedObjectContext = CoreDataStack.shared.persistantContainer.viewContext
+        let request: NSFetchRequest = fetchRequest()
+        var words: [Words] = []
+        
+        do {
+            words = try context.fetch(request)
+        } catch {
+            debugPrint(error)
+        }
+        return words
+    }
+}
